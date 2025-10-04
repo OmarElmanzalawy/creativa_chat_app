5df100c8dd7da5118d55cdc50435479d87f9e56b
Omar Elmanzalawy <omarmanz2002@gmail.com>
2025-10-04 13:07:08 +0300

    finish chat ui

--- lib/views/private_chat_screen.dart
+++ lib/views/private_chat_screen.dart
@@ -7,21 +7,52 @@ class PrivateChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Receiver name",style: TextStyle(color: Colors.white),),backgroundColor: Colors.blue,),
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text("Receiver name",style: TextStyle(color: Colors.white),),backgroundColor: Colors.blue,),
       body: Column(
         children: [
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: ListView(
-                padding: EdgeInsets.only(top:  15),
+            child: ListView(
+              padding: EdgeInsets.only(top:  15,right: 8,left: 8),
+              children: [
+                ChatBubble(isMe: true),
+                ChatBubble(isMe: false),
+              ],
+            )
+            ),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(8),
+              color: Colors.grey.shade50,
+              child: Row(
                 children: [
-                  ChatBubble(isMe: true),
-                  ChatBubble(isMe: false)
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Enter message",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25)
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25),
+                          borderSide: BorderSide(color: Colors.blue)
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10,),
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.blue
+                      ),
+                      child: IconButton(onPressed: (){}, icon: Icon(Icons.send,color: Colors.white,)),
+                  ),
+                const SizedBox(width: 10,),
                 ],
               ),
-            )
-            )
+            ),
         ],
       ),
     );
